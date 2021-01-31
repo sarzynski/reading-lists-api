ad9fefd8c48eb4a9140c06e495c0c1d494e63cc8
sarzynski <kamil.s.sarzynski@gmail.com>
2021-01-31 15:19:16 +0100

    add more tests

--- spec/requests/lists_request_spec.rb
+++ spec/requests/lists_request_spec.rb
@@ -25,6 +25,74 @@ RSpec.describe 'Lists', type: :request do
         expect(json).not_to be_empty
         expect(json['id']).to eq(list_id)
       end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+=begin
+    context 'when the record does not exist' do
+      let(:list_id) { 0 }
+    
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find List/)
+      end
+    end
+=end
+  describe 'POST /lists' do
+    let(:valid_attributes) { { name: 'Lista', created_by: '1' } }
+
+    context 'when the request is valid' do
+      before { post '/lists', params: valid_attributes }
+
+      it 'creates a list' do
+        expect(json['name']).to eq('Lista')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+  end
+=begin
+    context 'when the request is invalid' do
+      before { post '/lists', params: { name: 'Ksiazki'} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find List/)
+      end
+    end
+=end
+  describe 'PUT /lists/:id' do
+    let(:valid_attributes) { { name: 'Ksiazki'} }
+
+    context 'when the record exists' do
+      before { put "/lists/#{list_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /lists/:id' do
+    before { delete "/lists/#{list_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
     end
   end
 end
