99750ac81f707455f919b37922f88d46454a1f2b
sarzynski <kamil.s.sarzynski@gmail.com>
2021-01-31 12:05:46 +0100

    update list controller and add another test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,4 +5,33 @@ class ListsController < ApplicationController
     @lists = List.all
     json_response(@lists)
   end
+
+  def create
+    @list = List.create!(list_params)
+    json_response(@list, :created)
+  end
+
+  def show
+    json_response(@list)
+  end
+
+  def update
+    @list.update(list_params)
+    head :no_content
+  end
+
+  def destroy
+    @list.destroy
+    head :no_content
+  end
+
+  private
+
+  def list_params
+    params.permit(:name, :created_by)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 end
--- spec/requests/lists_request_spec.rb
+++ spec/requests/lists_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Lists", type: :request do
+RSpec.describe 'Lists', type: :request do
   let!(:lists) { create_list(:list, 10) }
   let(:list_id) { lists.first.id }
 
@@ -14,6 +14,17 @@ RSpec.describe "Lists", type: :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /lists/:id' do
+    before { get "/lists/#{list_id}" }
+
+    context 'when the record exists' do
+      it 'returns the list' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(list_id)
+      end
     end
   end
 end
