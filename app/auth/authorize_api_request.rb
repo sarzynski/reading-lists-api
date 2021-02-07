741064c393ae55ff24616b7e3130cfff1f16fe4b
sarzynski <kamil.s.sarzynski@gmail.com>
2021-02-07 12:41:45 +0100

    message class and import lib files

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -14,7 +14,7 @@ class AuthorizeApiRequest
   attr_reader :headers
 
   def user
-    @user ||= User.find(decode_auth_token[:user_id]) if decode_auth_token
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
   rescue ActiveRecord::RecordNotFound => e
     raise(ExceptionHandler::InvalidToken, ("#{Message.invalid_token} #{e.message}"))
   end
