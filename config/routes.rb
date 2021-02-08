403f974d052dc28ace93513d3ae4dcbdad21d280
sarzynski <kamil.s.sarzynski@gmail.com>
2021-02-08 19:09:03 +0100

    add api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists do
-    resources :books
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :lists, only: :index
+  end  
+    
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :lists do
+      resources :books
+    end
   end
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
---
+++ spec/requests/v2/lists_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "V2::Lists", type: :request do
+
+end
